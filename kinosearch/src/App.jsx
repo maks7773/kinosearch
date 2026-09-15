5c9a5c438b33ee5f272b2182017aae4c6db548bd
maks7773 <maksglad13102008@gmail.com>
2026-09-15 22:56:59 +0300

    2

--- kinosearch/src/App.jsx
+++ kinosearch/src/App.jsx
@@ -1,35 +1,47 @@
 import { useState } from "react";
 import MovieCard from "./MovieCard";
-import Header from "./Header"
+import Header from "./Header";
 import Footer from "./Footer";
 import "./App.css";
 
 function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null)
 
   async function searchMovies() {
-    const response = await fetch(
-      `http://www.omdbapi.com/?i=tt3896198&apikey=df0fd343&s=${query}`,
-    );
-    const data = await response.json();
-    console.log(data);
-    if (data.Search) {
-      setMovies(data.Search);
-    } else {
-      setMovies([]);
+    setError(null)
+    setLoading(true);
+        try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?i=tt3896198&apikey=df0fd343&s=${query}`,
+      );
+      const data = await response.json();
+      if (data.Search) {
+        setMovies(data.Search);
+      } else {
+        setMovies([]);
+        setError(data.Error || "Ничего не найдено");
+      }
+    } catch (error) {
+      setError("Something wrong")
+    } finally {
+      setLoading(false);
     }
   }
 
   return (
     <div>
-      <Header query={query} setQuery={setQuery} searchMovies={searchMovies}/>
+      <Header query={query} setQuery={setQuery} searchMovies={searchMovies} />
+      {loading && <div className="loading">Loading... ✌️</div>}
+      {error && <div className="error">{error}</div>}
       <div className="movies-grid">
         {movies.map((movie, index) => (
-          <MovieCard key={index} movie={movie}/>
+          <MovieCard key={index} movie={movie} />
         ))}
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- kinosearch/src/MovieCard.jsx
+++ kinosearch/src/MovieCard.jsx
@@ -1,7 +1,7 @@
 function MovieCard({ movie }) {
   return (
     <div className="card">
-      <img className="poster" src={movie.Poster} alt={movie.Title} />
+      {movie.Poster !== "N/A" ? <img className="poster" src={movie.Poster} alt={movie.Title} /> : <div>No poster</div>}    
       <h2 className="title">{movie.Title}</h2>
       <p className="year">{movie.Year}</p>
     </div>
