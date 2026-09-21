f6faf15205ad64860375abea0fb342e3ffb57006
maks7773 <maksglad13102008@gmail.com>
2026-09-21 12:51:48 +0300

    доделать по рейтингу надо

--- kinosearch/src/HomePage.jsx
+++ kinosearch/src/HomePage.jsx
@@ -3,33 +3,55 @@ import MovieCard from "./MovieCard";
 import Header from "./Header";
 import Footer from "./Footer";
 import "./App.css";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function HomePage() {
-    const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(null);
 
   async function searchMovies() {
-    setError(null)
+    setError(null);
     setLoading(true);
-        try {
+    try {
       const response = await fetch(
         `http://www.omdbapi.com/?i=tt3896198&apikey=df0fd343&s=${query}`,
       );
       const data = await response.json();
+
       if (data.Search) {
         setMovies(data.Search);
+        console.log(movies);
       } else {
         setMovies([]);
         setError(data.Error || "Ничего не найдено");
       }
     } catch (error) {
-      setError("Something wrong")
+      setError("Something wrong");
     } finally {
       setLoading(false);
     }
+  }
+
+  function sortByNew() {
+    const sortedMovies = [...movies].sort(
+      (a, b) => Number(b.Year) - Number(a.Year),
+    );
+    setMovies(sortedMovies);
+  }
+
+  function sortByOld() {
+    const sortedMovies = [...movies].sort(
+      (a, b) => Number(a.Year) - Number(b.Year),
+    );
+    setMovies(sortedMovies);
+  }
+
+  function sortByRate() {
+    const sortedMovies = [...movies].sort(
+      (a,b) => Number(b.imdbRating) - Number(a.imdbRating)
+    )
   }
 
   return (
@@ -37,6 +59,12 @@ function HomePage() {
       <Header query={query} setQuery={setQuery} searchMovies={searchMovies} />
       {loading && <div className="loading">Loading... ✌️</div>}
       {error && <div className="error">{error}</div>}
+      <div className="filters">
+        <button onClick={() => sortByNew()}>Сначала новые</button>
+        <button onClick={() => sortByOld()}>Сначала старые</button>
+        <button onClick={() => sortByRate()}>По рейтингу</button>
+      </div>
+
       <div className="movies-grid">
         {movies.map((movie, index) => (
           <MovieCard key={index} movie={movie} />
